4ba33323f236d7438d8ea96fb1cf4b49cccc2840
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-12-09 15:12:21 -0500

    added Skills component

---
+++ components/Skills.jsx
@@ -0,0 +1,72 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Skills = () => {
+  const skills = [
+    {
+      name: 'HTML',
+      image: 'html.png',
+    },
+    {
+      name: 'CSS',
+      image: 'css.png',
+    },
+    {
+      name: 'JavaScript',
+      image: 'javascript.png',
+    },
+    {
+      name: 'React',
+      image: 'react.png',
+    },
+    {
+      name: 'Tailwind',
+      image: 'tailwind.png',
+    },
+    {
+      name: 'NextJs',
+      image: 'nextjs.png',
+    },
+    {
+      name: 'Github',
+      image: 'github1.png',
+    },
+    {
+      name: 'MongoDB',
+      image: 'mongo.png',
+    },
+  ];
+
+  return (
+    <div className='w-full lg:h-screen p-2'>
+      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+        <p className='text-xl tracking-widest text-[#5651e5]'>Skills</p>
+        <h2 className='py-4'>What I Can Do</h2>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+          {skills.map((skill) => (
+            <div
+              key={skill.name}
+              className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-400'
+            >
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <Image
+                    src={`/assets/skills/${skill.image}`}
+                    width={64}
+                    height={64}
+                    alt=''
+                  />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>{skill.name}</h3>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
