6bc4d0541e779aaed27302d0b365512b2046a7a3
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-12-09 16:37:16 -0500

    added Contact component and info

---
+++ components/Contact.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const Contact = () => {
+  return (
+    <div className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+        <p className='uppercase text-xl tracking-widest text-[#5651e5]'>Contact</p>
+        <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-5 gap-8'>
+          {/* left */}
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='lg:p-4 h-full'>
+              <div>
+                <img
+                  className='rounded-xl hover:scale-105 ease-in duration-300'
+                  src='/assets/contact.jpg'
+                  alt=''
+                />
+              </div>
+              <div>
+                <h2 className='py-2'>Name here</h2>
+                <p>Front-End Developer</p>
+                <p className='py-4'>
+                  I am available for freelance or full-time positions. Contact me and let's talk.
+                </p>
+              </div>
+              <div>
+                <p className='uppercase pt-8'>Connect With Me</p>
+                <div className='flex items-center justify-between py-4'>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineMail />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <BsFillPersonLinesFill />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* right */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
