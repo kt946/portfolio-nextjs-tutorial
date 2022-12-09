9c407eda5f39ac45a917ef57e2406990acbf3a17
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-12-09 17:30:01 -0500

    updated Navbar links and added smooth scrolling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,41 +1,55 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener('scroll', handleShadow);
+  }, []);
+
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100] bg-[#ecf0f3]'>
+    <div className={`fixed w-full h-20 z-[100] bg-[#ecf0f3] ${shadow ? 'shadow-xl' : ''}`}>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image
-          src='/assets/navLogo.png'
-          alt=''
-          width={50}
-          height={50}
-        />
+        <Link href='/'>
+          <Image
+            src='/assets/navLogo.png'
+            alt=''
+            width={50}
+            height={50}
+          />
+        </Link>
         <div>
           <ul className='hidden md:flex'>
             <Link href='/'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
             </Link>
-            <Link href='/'>
+            <Link href='#about'>
               <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
             </Link>
-            <Link href='/'>
+            <Link href='#skills'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
             </Link>
-            <Link href='/'>
+            <Link href='#projects'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
             </Link>
-            <Link href='/'>
+            <Link href='#contact'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
             </Link>
           </ul>
@@ -80,16 +94,16 @@ const Navbar = () => {
               <Link href='/'>
                 <li className='py-4 text-sm'>Home</li>
               </Link>
-              <Link href='/'>
+              <Link href='#about'>
                 <li className='py-4 text-sm'>About</li>
               </Link>
-              <Link href='/'>
+              <Link href='#skills'>
                 <li className='py-4 text-sm'>Skills</li>
               </Link>
-              <Link href='/'>
+              <Link href='#projects'>
                 <li className='py-4 text-sm'>Projects</li>
               </Link>
-              <Link href='/'>
+              <Link href='#contact'>
                 <li className='py-4 text-sm'>Contact</li>
               </Link>
             </ul>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -38,7 +38,10 @@ const Skills = () => {
   ];
 
   return (
-    <div className='w-full lg:h-screen p-2'>
+    <div
+      id='skills'
+      className='w-full lg:h-screen p-2'
+    >
       <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
         <p className='uppercase text-xl tracking-widest text-[#5651e5]'>Skills</p>
         <h2 className='py-4'>What I Can Do</h2>
